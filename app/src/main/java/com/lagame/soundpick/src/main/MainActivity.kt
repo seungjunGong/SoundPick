4f7d037b2fee26c9fd9bcf3bdc1ad075d1c52bdd
seungjunGong <jjw5741@naver.com>
2023-12-31 23:30:30 +0900

    design: pick 메인 리사이클러뷰 추가
    버튼 클릭시마다 뷰를 그려 주기 위해
    리사이클러뷰로 구성

--- app/src/main/java/com/lagame/soundpick/src/main/MainActivity.kt
+++ app/src/main/java/com/lagame/soundpick/src/main/MainActivity.kt
@@ -3,18 +3,18 @@ package com.lagame.soundpick.src.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lagame.soundpick.R
+import com.lagame.soundpick.databinding.ActivityMainBinding
+import com.lagame.soundpick.src.main.config.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         var itemsData = ArrayList<PickItemsData>()
         var i = 1
         itemsData.add(PickItemsData("오전 9:30 - 오후 17:00 ", "23/12/19 - 23/12/31", true))
 
         val pickRcvAdapter = PickRcvAdapter(itemsData)
-        //binding.
-
+        binding.mainRcvPicks.adapter = pickRcvAdapter
     }
 }
