531115c1aa3a9143a2a8e9bed3f4949876e54f71
seungjunGong <jjw5741@naver.com>
2024-01-08 21:46:36 +0900

    design: 설정 페이지 구성
    설정 페이지를 구성함
    전체적인 UI margin 과 크기 수정필요
    특히 2번째 박스 부분 수정
    
    close #1

---
+++ app/src/main/java/com/lagame/soundpick/src/main/UpdateActivity.kt
@@ -0,0 +1,12 @@
+package com.lagame.soundpick.src.main
+
+import android.os.Bundle
+import com.lagame.soundpick.databinding.ActivityUpdateBinding
+import com.lagame.soundpick.src.main.config.BaseActivity
+
+class UpdateActivity : BaseActivity<ActivityUpdateBinding>(ActivityUpdateBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        (super.onCreate(savedInstanceState))
+
+    }
+}
